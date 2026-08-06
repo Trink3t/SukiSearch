b77f2c6319bb4bf02177d5fd108b88726cf9c056
Aldrin Aureus <aldrinaureus.1050@gmail.com>
2026-08-06 11:59:08 +0800

    feat: Update .env.example for SukiSearch-Backend and add AuthService class

---
+++ Backend/app/Services/AuthService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+class AuthService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function login() {}
+
+    public function logout() {}
+
+    public function register() {}
+}
